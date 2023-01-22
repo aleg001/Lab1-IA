c944b82de376f82ca921d9d6f88780139ba0e3a7
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2023-01-22 12:52:19 -0600

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -9,7 +9,6 @@ class Node:
     self.succesors = None
     self.explored = False
 
-
 class Tree:
   def __init__(self, path:list[list]) -> None:
     for row in path:
@@ -25,13 +24,29 @@ class Tree:
     for y in range(len(path)):
       for x in range(len(path)):
         succesors = []
-        succesors.append(NodeBuffer[y][x+1]) if x < len(path) else ''
-        succesors.append(NodeBuffer[y][x-1]) if x > 0 else ''
-        succesors.append(NodeBuffer[y+1][x+1]) if x < len(path) and x < len(path) else ''
-        succesors.append(NodeBuffer[y+1][x-1]) if x > 0 and y < len(path) else ''
-        succesors.append(NodeBuffer[y-1][x+1]) if y > 0 and x < len(path) else ''
-        succesors.append(NodeBuffer[y-1][x-1]) if x > 0 and y > 0 else ''
-        succesors.append(NodeBuffer[y-1][x]) if y > 0 else ''
-        succesors.append(NodeBuffer[y-1][x]) if y < len(path) else ''
+        if x < len(path) - 1:                       succesors.append(NodeBuffer[y][x+1]) 
+        if x > 0:                                   succesors.append(NodeBuffer[y][x-1])
+        if x < len(path) - 1 and y < len(path) - 1: succesors.append(NodeBuffer[y+1][x+1])
+        if x > 0 and y < len(path) - 1:             succesors.append(NodeBuffer[y+1][x-1])
+        if y > 0 and x < len(path) - 1:             succesors.append(NodeBuffer[y-1][x+1])
+        if x > 0 and y > 0:                         succesors.append(NodeBuffer[y-1][x-1])
+        if y > 0:                                   succesors.append(NodeBuffer[y-1][x])
+        if y < len(path) - 1:                       succesors.append(NodeBuffer[y-1][x])
 
         NodeBuffer[y][x].succesors = succesors
+
+path = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 2, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 2, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0],
+  [0, 0, 0, 2, 0, 0, 2, 0, 0, 0],
+  [0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+  [0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+
+x = Tree(path)
