d61b3efc68e14f71d2294debbbfe4a3d95190cde
Ale Gomez <gom20347@uvg.edu.gt>
2023-01-23 17:58:00 -0600

    created abstract method

--- Framework.py
+++ Framework.py
@@ -1,12 +1,11 @@
-from typing import TypeVar, Generic
-from abc import ABC
-
-T = TypeVar("T")
+from abc import ABC, abstractmethod
 
 
-class Framework(Generic[T], ABC):
+class Framework(ABC):
+    @abstractmethod
     def __init__(self, name: str):
         self._name = name
 
+    @abstractmethod
     def name(self) -> str:
         return self._name
