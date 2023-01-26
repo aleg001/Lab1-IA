fd7086ce3c8aba7f64cd81186cccb80e04ee8c47
Ale Gomez <gom20347@uvg.edu.gt>
2023-01-26 00:04:35 -0600

    se finaliza lector de imagenes y se inician pruebas

--- GraphSearch.py
+++ GraphSearch.py
@@ -4,141 +4,22 @@ https://www.hackerearth.com/practice/notes/extracting-pixel-values-of-an-image-i
 https://stackoverflow.com/questions/12995434/representing-and-solving-a-maze-given-an-image
 
 """
-
-# ===============================================================
-# CÓDIGO OBTENIDO DEL CURSO GRÁFICOS POR COMPUTADOR 2022
-# AUTOR ORIGINAL: DENNIS ALDANA
-# MODIFICACIONES POR: ALEJANDRO GÓMEZ, MARÍA ISABEL SOLANO, DIEGO CÓRDOVA
-# ===============================================================
-
 from PIL import Image
-import struct
+from ImageProcessing import *
+from BMP_RW import *
+from BFS import *
+from DFS import *
 
 
-def char(c):
-    """
-    Input: requires a size 1 string
-    Output: 1 byte of the ascii encoded char
-    """
-    return struct.pack("=c", c.encode("ascii"))
+Resultado = CargaImagenes("l1")
+# print(Resultado)
 
+BFSTest = BFS(Resultado)
+print(BFSTest.path)
+for i in BFSTest.MtoGtoM:
+    print(i)
 
-def word(w):
-    """
-    Input: requires a number such that (-0x7fff - 1) <= number <= 0x7fff
-           ie. (-32768, 32767)
-    Output: 2 bytes
-    Example:
-    >>> struct.pack('=h', 1)
-    b'\x01\x00'
-    """
-    return struct.pack("=h", w)
+print("step cost: ", BFSTest.stepCost())
+print("path cost: ", BFSTest.pathCost(BFSTest.path))
 
-
-def dword(d):
-    """
-    Input: requires a number such that -2147483648 <= number <= 2147483647
-    Output: 4 bytes
-    Example:
-    >>> struct.pack('=l', 1)
-    b'\x01\x00\x00\x00'
-    """
-    return struct.pack("=l", d)
-
-
-def color(r, g, b):
-    """
-    Input: each parameter must be a number such that 0 <= number <= 255
-           each number represents a color in rgb
-    Output: 3 bytes
-    Example:
-    >>> bytes([0, 0, 255])
-    b'\x00\x00\xff'
-    """
-    return bytes([b, g, r])
-
-
-# ===============================================================
-# BMP
-# ===============================================================
-
-
-def writebmp(filename, width, height, pixels):
-    f = open(filename, "bw")
-
-    # File header (14 bytes)
-    f.write(char("B"))
-    f.write(char("M"))
-    f.write(dword(14 + 40 + width * height * 3))
-    f.write(dword(0))
-    f.write(dword(14 + 40))
-
-    # Image header (40 bytes)
-    f.write(dword(40))
-    f.write(dword(width))
-    f.write(dword(height))
-    f.write(word(1))
-    f.write(word(24))
-    f.write(dword(0))
-    f.write(dword(width * height * 3))
-    f.write(dword(0))
-    f.write(dword(0))
-    f.write(dword(0))
-    f.write(dword(0))
-
-    # Pixel data (width x height x 3 pixels)
-    for x in range(height):
-        for y in range(width):
-            f.write(pixels[x][y])
-    f.close()
-
-
-# Constantes de colores en una clase:
-class Colores:
-    ROJO = (255, 0, 0)
-    VERDE = (0, 255, 0)
-    NEGRO = (0, 0, 0)
-    BLANCO = (255, 255, 255)
-
-
-# Se abre imagen y se define el tamaño:
-imagen = Image.open("l1.bmp")
-pixelesGrid = imagen.load()
-ancho = imagen.size[0]
-altura = imagen.size[1]
-
-# Definicion de colores en una clase:
-class PosicionesDisponibles:
-    espaciosDisponibles = Colores.BLANCO
-    puntoInicio = Colores.ROJO
-    metaPuntos = Colores.VERDE
-    espaciosNoDisponibles = Colores.NEGRO
-
-
-# Lista vacia a llenar con puntos de meta:
-# -- Se llena con tuplas de coordenadas
-PuntosDeMeta = []
-# Se usa una tupla para almacenar coordenadas de inicio
-puntoRojo = ()
-
-
-for i in range(0, ancho):
-    for j in range(0, altura):
-        match pixelesGrid[i, j]:
-            case PosicionesDisponibles.metaPuntos:
-                PuntosDeMeta.append((i, j))
-            case PosicionesDisponibles.puntoInicio:
-                puntoRojo = (i, j)
-
-
-# Funcion para detectar paredes (ver si la posicion si es valida)
-def detectarParedes(x, y, w, h):
-    match pixelesGrid[x, y]:
-        case PosicionesDisponibles.espaciosDisponibles:
-            return True
-        case PosicionesDisponibles.espaciosNoDisponibles:
-            return False
-        case PosicionesDisponibles.puntoInicio:
-            return True
-        case PosicionesDisponibles.metaPuntos:
-            return True
+print("Pass test: ", BFSTest.test)
--- ImageProcessing.py
+++ ImageProcessing.py
@@ -19,56 +19,87 @@ from pixelate import *
 # Se importa clase de colores
 from Colores import *
 
+
 # Constantes
 Pixel = 15
-path = "l1.bmp"
+path = "l1"
 endResult = "result.bmp"
 # Se llama a funcion de pixelate, recibiendo como parametro la imagen input, la imagen output y el tamaño de los pixeles.
-pixelate(path, endResult, Pixel)
+
 
 # Se define paleta de colores
 colores = [Colores.ROJO, Colores.VERDE, Colores.NEGRO, Colores.BLANCO]
 
-# La imagen resultado se carga
-endResultOpened = Image.open(endResult)
-# Se cambia a paleta de colores y se cuantiza a estos
-imagen = endResultOpened.convert("RGB", palette=colores)
-im1 = endResultOpened.quantize(4)
 
-# Se obtiene la información de la imagen
-informacionFoto = endResultOpened.getdata()
-imagen4Colores = []
+def CargaImagenes(path):
+    endResult = path + "result.bmp"
+    path = path + ".bmp"
+    pixelate(path, endResult, Pixel)
+    # La imagen resultado se carga
+    endResultOpened = Image.open(endResult)
+    # Se cambia a paleta de colores y se cuantiza a estos
+    imagen = endResultOpened.convert("RGB", palette=colores)
+    im1 = endResultOpened.quantize(4)
+
+    # Se obtiene la información de la imagen
+    informacionFoto = endResultOpened.getdata()
+    imagen4Colores = []
+
+    # Se reduce a 4 colores la imagen
+    index = 0
+    # Se itera sobre la cantidad de pixeles
+    while index < len(informacionFoto):
+        item = informacionFoto[index]
+        # Se chequea el rango rgb 0,200 para saber si es negro segun el pixel
+        if all(0 <= x <= 200 for x in item):
+            imagen4Colores.append(Colores.NEGRO)
+        # Se chequea el rango rgb 100,120 para saber si es rojo segun el pixel
+        elif all(100 <= x <= 120 for x in item):
+            imagen4Colores.append(Colores.ROJO)
+        # Se chequea el rango rgb 150,170 para saber si es verde segun el pixel
+        elif all(150 <= x <= 170 for x in item):
+            imagen4Colores.append(Colores.VERDE)
+        # Se chequea el rango rgb 200,255 para saber si es blanco segun el pixel
+        elif all(200 <= x <= 255 for x in item):
+            imagen4Colores.append(Colores.BLANCO)
+        else:
+            imagen4Colores.append(item)
+        # Se sigue al siguiente pixel
+        index += 1
+
+    # Se agrega la data a la imagen
+    endResultOpened.putdata(imagen4Colores)
+    # Se despliega imagen
+    # endResultOpened.show()
+    # Se guarda imagen
+    endResultOpened.save(endResult)
+
+    # Se convierte en un array la imagen. Referencia: https://thecleverprogrammer.com/2021/06/08/convert-image-to-array-using-python/#:~:text=Converting%20an%20Image%20to%20Array,pip%20install%20Pillow
+    data = np.array(endResultOpened).tolist()
+    # print(data)
+
+    dataNormal = []
+
+    # Proceso de recorrer los pixeles para obtener los colores y asignarlos en una nueva lista
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+
+            if (data[i][j][0]) == 0:
+                dataNormal.append(0)
+
+            if (data[i][j][0]) == 255:
+                dataNormal.append(1)
+
+            if (data[i][j][0]) == 254:
+                dataNormal.append(2)
+
+            if (data[i][j][0]) == 41:
+                dataNormal.append(3)
+
+    # Por si necesitamos usar array de numpy!
+    listToArray = np.array(dataNormal)
+
+    return dataNormal
 
 
-# Se reduce a 4 colores la imagen
-index = 0
-# Se itera sobre la cantidad de pixeles
-while index < len(informacionFoto):
-    item = informacionFoto[index]
-    # Se chequea el rango rgb 0,200 para saber si es negro segun el pixel
-    if all(0 <= x <= 200 for x in item):
-        imagen4Colores.append(Colores.NEGRO)
-    # Se chequea el rango rgb 100,120 para saber si es rojo segun el pixel
-    elif all(100 <= x <= 120 for x in item):
-        imagen4Colores.append(Colores.ROJO)
-    # Se chequea el rango rgb 150,170 para saber si es verde segun el pixel
-    elif all(150 <= x <= 170 for x in item):
-        imagen4Colores.append(Colores.VERDE)
-    # Se chequea el rango rgb 200,255 para saber si es blanco segun el pixel
-    elif all(200 <= x <= 255 for x in item):
-        imagen4Colores.append(Colores.BLANCO)
-    else:
-        imagen4Colores.append(item)
-    # Se sigue al siguiente pixel
-    index += 1
-
-# Se agrega la data a la imagen
-endResultOpened.putdata(imagen4Colores)
-# Se despliega imagen
-endResultOpened.show()
-# Se guarda imagen
-endResultOpened.save(endResult)
-
-# Se convierte en un array la imagen. Referencia: https://thecleverprogrammer.com/2021/06/08/convert-image-to-array-using-python/#:~:text=Converting%20an%20Image%20to%20Array,pip%20install%20Pillow
-data = np.asarray(endResultOpened)
-print(data)
+CargaImagenes(path)
